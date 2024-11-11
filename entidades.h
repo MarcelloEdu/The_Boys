bb2f2289f34f3eaa35578e5455eb3677887251a1
MarcelloEdu <eduardomarcello912@gmail.com>
2024-11-11 16:34:36 -0300

    [INIT] adicionando os meus arquivos

---
+++ entidades.h
@@ -0,0 +1,50 @@
+#ifndef ENTIDADES_H
+#define ENTIDADES_H
+
+#define T_INICIO 0
+#define T_FIM 10000
+#define N_HABILIDADES 10
+#define TAMANHO_MUNDO 20000
+#define N_HEROIS (N_HABILIDADES * 5)
+#define N_BASES (N_HEROIS / 6)
+#define N_MISSOES (T_FIM / 100)
+
+struct coordenadas
+{
+    int x;
+    int y;
+};
+
+struct base
+{
+    int id;
+    int lotacao;
+    struct conjunto *presentes;
+    struct fila *espera;
+    struct coordenadas *coord;
+};
+
+
+struct heroi 
+{
+    int id;
+    int paciencia;
+    int velocidade;
+    int experiencia;
+    int base_atual;
+    struct conjunto *habilidades;
+};
+
+struct missao 
+{
+    int id;
+    struct conjunto *habilidades;
+    struct coordenadas *coordenadas;
+};
+
+int aleat(int min, int max);
+struct heroi inicia_heroi(struct heroi *heroi, int id);
+struct base inicia_base(struct base *b, int id);
+void inicia_missao(struct missao *missao, int id);
+ 
+#endif
