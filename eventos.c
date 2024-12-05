c355d92585f2bbcc7b7c104e3d553d6502dffe38
MarcelloEdu <eduardomarcello912@gmail.com>
2024-12-05 02:57:19 -0300

    [ADD] prototipo do evento missao

--- eventos.c
+++ eventos.c
@@ -10,25 +10,9 @@
 #include "mundo.h"
 #include "complementos.h"
 
-/*
-{
-CHEGA (T, H, B):
 
-atualiza base de H
-
-se há vagas em B e a fila de espera em B está vazia:
-    espera = true
-senão:
-    espera = (paciência de H) > (10 * tamanho da fila em B)
-
-se espera:
-    cria e insere na LEF o evento ESPERA (agora, H, B)
-senão:
-    cria e insere na LEF o evento DESISTE (agora, H, B)
-}
-*/
 void chega(int tempo, struct heroi *h, struct base *b, struct fprio_t *fprio){
-    h->base_atual = b->id;
+    h->base_atual->id = b->id; //atualiza base de H
 
     if (b->lotacao > cjto_card(b->presentes) && fila_vazia(b->espera))
     {
@@ -65,13 +49,6 @@ void chega(int tempo, struct heroi *h, struct base *b, struct fprio_t *fprio){
     }
 }
 
-
-/*
-ESPERA (T, H, B):
-
-adiciona H ao fim da fila de espera de B
-cria e insere na LEF o evento AVISA (agora, B)
-*/
 void espera(int tempo, struct heroi *heroi, struct base *base, struct fprio_t *fprio) {
     enqueue(base->espera, heroi->id);
     CriaInsere(tempo, TIPO_AVISA, base, NULL, fprio);
@@ -82,14 +59,8 @@ void espera(int tempo, struct heroi *heroi, struct base *base, struct fprio_t *f
             fila_tamanho(base->espera));
 }
 
-/*
-DESISTE (T, H, B):
-
-escolhe uma base destino D aleatória
-cria e insere na LEF o evento VIAJA (agora, H, D)
-*/
 void desiste(int tempo, struct heroi *h, struct base *b, struct fprio_t *fprio){
-    h->base_destino = rand() % N_BASES;
+    h->base_destino->id = rand() % N_BASES;
     CriaInsere(tempo, TIPO_VIAJA, h, b, fprio);
     printf("%6d: DESISTE HEROI %2d BASE %d \n",
             tempo, 
@@ -97,14 +68,6 @@ void desiste(int tempo, struct heroi *h, struct base *b, struct fprio_t *fprio){
             b->id);
 }
 
-/*
-AVISA (T, B):
-
-enquanto houver vaga em B e houver heróis esperando na fila:
-    retira primeiro herói (H') da fila de B
-    adiciona H' ao conjunto de heróis presentes em B
-    cria e insere na LEF o evento ENTRA (agora, H', B)
-*/
 void avisa(int tempo, struct base *b, struct fprio_t *fprio){
     printf("%6d: AVISA PORTEIRO BASE %d (%d/%d) \n",
             tempo,
@@ -124,14 +87,8 @@ void avisa(int tempo, struct base *b, struct fprio_t *fprio){
     }
 }
 
-/*
-ENTRA (T, H, B):
-
-calcula TPB = tempo de permanência na base:
-    TPB = 15 + paciência de H * aleatório [1...20]
-cria e insere na LEF o evento SAI (agora + TPB, H, B)
-*/
 void entra(int tempo, struct heroi *h, struct base *b, struct fprio_t *fprio){
+    cjto_uniao(b->habilidades, h->habilidades);//adiciona habilidades do heroi ao conjunto de habilidades da base
     int TPB = 15 + h->paciencia * (rand() % 20 + 1);
     tempo += TPB;
     CriaInsere(tempo, TIPO_SAI, h, b, fprio);
@@ -146,17 +103,10 @@ void entra(int tempo, struct heroi *h, struct base *b, struct fprio_t *fprio){
             TPB);
 }
 
-/*
-SAI (T, H, B):
-
-retira H do conjunto de heróis presentes em B
-escolhe uma base destino D aleatória
-cria e insere na LEF o evento VIAJA (agora, H, D)
-cria e insere na LEF o evento AVISA (agora, B)
-*/
 void sai(int tempo, struct heroi *h, struct base *b, struct fprio_t *fprio){
+    cjto_retira(b->habilidades, h->habilidades);
     cjto_retira(b->presentes, h->id);
-    h->base_destino = rand() % N_BASES;
+    h->base_destino->id = rand() % N_BASES;
     CriaInsere(tempo, TIPO_VIAJA, h, b, fprio);
     CriaInsere(tempo, TIPO_AVISA, b, NULL, fprio);
 
@@ -168,14 +118,6 @@ void sai(int tempo, struct heroi *h, struct base *b, struct fprio_t *fprio){
             b->lotacao);
 }
 
-/*
-VIAJA (T, H, D):
-
-calcula duração da viagem:
-    distância = distância cartesiana entre a base atual de H e a base D 
-    duração = distância / velocidade de H
-cria e insere na LEF o evento CHEGA (agora + duração, H, D)
-*/
 void viaja(int tempo, struct heroi *h, struct base *b, struct mundo *mundo, struct fprio_t *fprio){
     
     int dist = distancia_euclidiana(b->coord, h->base_destino->coord);
@@ -209,16 +151,65 @@ senão:
     cria e insere na LEF o evento MISSAO (T + 24*60, M) para o dia seguinte
 */
 void missao(int tempo, struct missao *m, struct mundo *mundo, struct fprio_t *fprio){
+    
+    struct base *bases_aptas[N_BASES];//vetor de bases aptas a cumprir a missão
+    int aptas = 0;
+    int dia_seguinte = tempo + 24*60;
+
+    printf("%6d: MISSAO %d HAB REQ: ",
+            tempo,
+            m->id);
+
+    cjto_imprime(m->habilidades);
+
+    //verifica se a base possui as habilidades necessárias para cumprir a missão
+    for(int i = 0; i < N_BASES; i++){
+        struct base *b = &mundo->bases[i]; //base atual
+
+        if(cjto_contem(b->habilidades, m->habilidades)){
+            bases_aptas[aptas] = b;//adiciona base atual (marcada como apta) ao vetor
+            aptas++;
+        }
+    }
+
+    //se não houver bases aptas a cumprir a missão, adia
+    if(aptas == 0){
+        printf("%6d: MISSAO %d IMPOSSIVEL \n",
+                tempo,
+                m->id);
+        CriaInsere(dia_seguinte, TIPO_MISSAO, m, NULL, fprio);
+        return;
+    }
+
+    quicksort(bases_aptas, 0, aptas-1, m);//ordena bases aptas pela distância euclidiana
+
+    //base mais próxima da missão
+    int BMP = bases_aptas[0]->id;
+
+    //marca missão como cumprida
+    mundo->missoes_cumpridas++;
+    printf("%6d: MISSAO %2d CUMPRIDA HEROIS %d \n",
+            tempo,
+            m->id,
+            cjto_card(bases_aptas[BMP]->presentes));
+
+    //para cada heroi presente na base mais próxima
+    for(int i = 0; i < cjto_card(bases_aptas[BMP]->presentes); i++){
+
+        int heroi_id = cjto_card(bases_aptas[BMP]->presentes);
+        struct heroi *h = &mundo->herois[heroi_id];
+
+        float risco = m->perigo / (h->paciencia + h->experiencia + 1.0);
+
+        if(risco > aleat(0, 30)){
+            CriaInsere(tempo, TIPO_MORRE, h, bases_aptas[BMP], fprio);
+        }else{
+            h->experiencia++;
+        }
+    }
 
 }
 
-/*
-MORRE (T, H, B):
-
-retira H do conjunto de heróis presentes em B
-muda o status de H para morto 
-cria e insere na LEF o evento AVISA (agora, B)
-*/
 void morre(int tempo, struct heroi *h, struct base *b, struct fprio_t *fprio){
     cjto_retira(b->presentes, h->id);
     h->status = 1;
@@ -236,5 +227,3 @@ FIM (T):
 void fim(int tempo, struct fprio_t *fprio){
 
 }
-
-
